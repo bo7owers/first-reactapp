0a6746c37ab027c320e5d6fc8005404e8835a161
René Torres <7orresrene@users.noreply.github.com>
2021-05-20 23:24:47 -0400

    First message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello, friend.
         </p>
         <a
           className="App-link"
@@ -18,6 +18,10 @@ function App() {
           Learn React
         </a>
       </header>
+      <body
+      className="body">
+        This is my custom text 
+      </body>
     </div>
   );
 }
